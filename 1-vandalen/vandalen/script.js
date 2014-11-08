7125cd86a6b3a093a18c72ab813672a70d096e1a
hj222hi <hj222hi@student.lnu.se>
2014-11-08 16:11:50 +0100

    4 klar bara felmeddelande kvar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,30 @@
-"use strict";
+ "use strict";
 
 var makePerson = function(persArr){
+  
+  var names = persArr.map(function (persArr){ return persArr.name;}).sort(function(a,b){return a.localeCompare(b)}).join(", ").toString();
+  var allages = persArr.map(function (persArr){ return persArr.age});
+  var res = {};
+  var maxage = Math.max.apply(Math, allages);
+  var minage = Math.min.apply(Math, allages);
+  var avrage = Math.round(allages.reduce(function(a, b) { return a + b }) / persArr.length);
+              
 
 
-	// Din kod här...
 
-}
+console.log(avrage);
+res = {averageAge:avrage, maxAge:maxage, minAge:minage, names:names};
+console.log(res);
+return res;
+ };
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
+var result = makePerson(data);
+
+
+
+
+
+ 
+
 
