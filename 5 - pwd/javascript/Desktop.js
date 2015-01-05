33d4c1d6430e240af5b45b5f0ba7534c6564cab2
Hampus Jarleborn <hj222hi@student.lnu.se>
2015-01-05 10:41:36 +0000

    tror att betyg 1 är klart nu :dance:

--- 5 - pwd/javascript/Desktop.js
+++ 5 - pwd/javascript/Desktop.js
@@ -1,4 +1,129 @@
-function desktop()
-{
-    
-}
+// var theWindow = {
+    var xhr = new XMLHttpRequest(); 
+  var o = 0;
+   
+     function Window(func){
+        
+        var template = document.querySelector("#template");
+        var windowTemplate = template.content.querySelector(".window");
+        var w = windowTemplate.cloneNode(true);
+        
+        document.querySelector('#wrapper').appendChild(
+        w);
+        func;
+    };
+       
+       function SetUpImages(){
+        xhr.onreadystatechange = function(){
+         var imgArray;  
+         var load = document.querySelectorAll(".hidden");
+         var load2 = document.querySelectorAll(".load");
+         var load3 = document.querySelectorAll(".loadtext");
+         
+            //  load[o].classList.add("show");
+
+                if(xhr.readyState === 4 &&xhr.status !== 400){
+                      console.log(load[o]);
+                    load[o].classList.add("hidden")
+                    load2[o].classList.add("hidden")
+                    load3[o].classList.add("hidden")
+                     
+                   imgArray = JSON.parse(xhr.responseText);
+                //   console.log(imgArray);
+                  
+                    var windowCenter = document.querySelectorAll(".windowcenter");
+                    console.log(windowCenter[o]);
+                   
+                    for(var i = 0; i < imgArray.length; i++){
+                        var tumbHolder = document.createElement("div");
+                        tumbHolder.setAttribute("class","tumbholder")
+                        
+                        var tumb = document.createElement("img");
+                        var a = document.createElement("a");
+                        a.setAttribute("href","#")
+                        a.setAttribute("class","atag")
+                        
+                        windowCenter[o].appendChild(a);
+                       
+                        a.appendChild(tumbHolder);
+                        tumb.setAttribute("class",""+i+"")
+                          tumb.setAttribute("class","tumb")
+                        tumb.setAttribute("src",""+imgArray[i].thumbURL+"")
+                       
+                        tumbHolder.appendChild(tumb);
+                        var maxWidth = 75;
+                        var maxHeight = 50;
+                
+                        if(imgArray[i].thumbWidth > maxWidth){
+                            maxWidth = imgArray[i].thumbWidth;
+                            
+                             }
+                        
+                        if(imgArray[i].thumbHeight > maxHeight){
+                            maxHeight = imgArray[i].thumbHeight;
+                            // console.log(maxHeight);
+                             }
+                       
+                    tumbHolder.style.height = ""+maxHeight+"px";
+                    tumbHolder.style.width = ""+maxWidth+"px";
+                    //  console.log(imgArray[i].URL);
+              
+              setBackground(tumbHolder, imgArray[i].URL);
+             
+                    }
+                        console.log(o);
+                        loadCloseButton(o);
+                        o++ 
+                        tumbHolder.appendChild(tumb);
+                        // var atagArray = document.querySelector(".atag");
+                        //  for(var k = 0; k < imgArray.length; k++){
+                        // atagArray[k].onclick =presser(imgArray[k]);
+                    // }
+                        
+                       
+                }
+                // console.log(windowCenter);
+                 
+            };
+           xhr.open('GET','http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/', true); 
+        xhr.send(null);
+            
+        // var wName = document.getElementsByClassName("windowname") 
+        // for(var n = 0; n< wName.length; n++){
+        //   wName[n].innerHTML("Pics")  
+        // };
+            
+            
+    };
+     function setBackground (tumbHolder, url){
+        //  console.log(tumbHolder);
+            tumbHolder.addEventListener("click", function(){
+                 var body = document.getElementsByTagName("body");
+                //  console.log(body);
+                  body[0].style.backgroundImage = 'url('+url+')';
+            })
+         
+     };
+     
+     function loadCloseButton(windowNumber){
+        var allTheWindows = document.querySelectorAll(".window");
+        var theWindowThatsAboutToBeClosed = allTheWindows[windowNumber];
+         var closeButton = document.querySelectorAll(".closeb");
+        //  console.log(closeButton);
+         closeButton[windowNumber].addEventListener("click", function(theWindowThatsAboutToBeClosed){
+            //  console.log(theWindowThatsAboutToBeClosed);
+             allTheWindows[windowNumber].style.display = 'none';
+             
+             
+         });
+     }
+    //  presser: function(imgA){
+        
+    //         var body = document.getElementsByTagName('body')[0];
+    //             body.style.backgroundImage = "url("+imgA.URL+")";
+            
+        
+                
+           
+    //     },
+    //  };
--- 5 - pwd/javascript/Window.js
+++ 5 - pwd/javascript/Window.js
@@ -1,99 +1,118 @@
 "use strict"
 var theWindow = {
-     xhr: new XMLHttpRequest(), 
+    //  xhr: new XMLHttpRequest(), 
 
-    Window: function(){
+    // Window: function(){
        
-        var template = document.querySelector("#template");
-        var windowTemplate = template.content.querySelector(".window");
-        var w = windowTemplate.cloneNode(true);
+    //     var template = document.querySelector("#template");
+    //     var windowTemplate = template.content.querySelector(".window");
+    //     var w = windowTemplate.cloneNode(true);
         
-        document.querySelector('#wrapper').appendChild(
-        w);
-    },
+    //     document.querySelector('#wrapper').appendChild(
+    //     w);
+    // },
     
     setButtonPics: function(){
         
          document.getElementById("picslink").onclick = function(){ 
-              
-              
-           theWindow.Window();
-           theWindow.setUpImages();
+     
+        var window1 = new Window(SetUpImages());
+        // var imageSetter = new SetUpImages();
+        window1;
+        // imageSetter;
         };
     },
   
-        setUpImages: function(){
-            console.log("hoj");
-         theWindow.xhr.onreadystatechange = function(){
-         var imgArray;      
-                if(theWindow.xhr.readyState === 4 && theWindow.xhr.status !== 400){
-                   imgArray = JSON.parse(theWindow.xhr.responseText);
-                   console.log(imgArray);
+    //     setUpImages: function(){
+    //         console.log("hoj");
+    //      theWindow.xhr.onreadystatechange = function(){
+    //      var imgArray;  
+    //      var load = document.getElementsByClassName("hidden");
+    //      for(var l = 0; l< load.length; l++){
+    //          load[l].classList.add("show");
+    //      }
+    //             if(theWindow.xhr.readyState === 4 && theWindow.xhr.status !== 400){
+    //                   var show = document.getElementsByClassName("show");
+    //                   for(var m = 0; m < show.length; m++){
+    //                         show[m].classList.remove("show");
+    //                         load[m].classList.remove("show");
+    //                  }
+    //               imgArray = JSON.parse(theWindow.xhr.responseText);
+    //               console.log(imgArray);
                     
-                    var windowCenter = document.querySelector(".windowcenter");
-                    console.log(windowCenter);
+    //                 var windowCenter = document.querySelector(".windowcenter");
+    //                 console.log(windowCenter);
                      
-                    for(var i = 0; i < imgArray.length; i++){
-                        var tumbHolder = document.createElement("div");
-                        tumbHolder.setAttribute("class","tumbholder")
+    //                 for(var i = 0; i < imgArray.length; i++){
+    //                     var tumbHolder = document.createElement("div");
+    //                     tumbHolder.setAttribute("class","tumbholder")
                         
-                        var tumb = document.createElement("img");
-                        var a = document.createElement("a");
-                        a.setAttribute("href","#")
-                        a.setAttribute("class","atag")
-                        windowCenter.appendChild(a);
-                        a.appendChild(tumbHolder);
-                        tumb.setAttribute("class",""+i+"")
-                          tumb.setAttribute("class","tumb")
-                        tumb.setAttribute("src",""+imgArray[i].thumbURL+"")
+    //                     var tumb = document.createElement("img");
+    //                     var a = document.createElement("a");
+    //                     a.setAttribute("href","#")
+    //                     a.setAttribute("class","atag")
+    //                     windowCenter.appendChild(a);
+    //                     a.appendChild(tumbHolder);
+    //                     tumb.setAttribute("class",""+i+"")
+    //                       tumb.setAttribute("class","tumb")
+    //                     tumb.setAttribute("src",""+imgArray[i].thumbURL+"")
                        
-                        tumbHolder.appendChild(tumb);
-                        var maxWidth = 75;
-                        var maxHeight = 50;
+    //                     tumbHolder.appendChild(tumb);
+    //                     var maxWidth = 75;
+    //                     var maxHeight = 50;
                 
-                        if(imgArray[i].thumbWidth > maxWidth){
-                            maxWidth = imgArray[i].thumbWidth;
+    //                     if(imgArray[i].thumbWidth > maxWidth){
+    //                         maxWidth = imgArray[i].thumbWidth;
                             
-                             }
+    //                          }
                         
-                        if(imgArray[i].thumbHeight > maxHeight){
-                            maxHeight = imgArray[i].thumbHeight;
-                            console.log(maxHeight);
-                             }
+    //                     if(imgArray[i].thumbHeight > maxHeight){
+    //                         maxHeight = imgArray[i].thumbHeight;
+    //                         console.log(maxHeight);
+    //                          }
                        
-                    tumbHolder.style.height = ""+maxHeight+"px";
-                    tumbHolder.style.width = ""+maxWidth+"px";
-                     console.log(imgArray[i].URL);
-                   theWindow.presser(imgArray)
-                    }
+    //                 tumbHolder.style.height = ""+maxHeight+"px";
+    //                 tumbHolder.style.width = ""+maxWidth+"px";
+    //                  console.log(imgArray[i].URL);
+              
+    //           theWindow.setBackground(tumbHolder, imgArray[i].URL);
+    //                 }
                         
-                        tumbHolder.appendChild(tumb);
-                        var atagArray = document.querySelector(".atag");
-                         for(var k = 0; k < imgArray.length; k++){
-                        atagArray[k].onclick = theWindow.presser(imgArray[k]);
-                    }
+    //                     tumbHolder.appendChild(tumb);
+    //                     var atagArray = document.querySelector(".atag");
+    //                      for(var k = 0; k < imgArray.length; k++){
+    //                     atagArray[k].onclick = theWindow.presser(imgArray[k]);
+    //                 }
                         
                        
-                }
-            };
-            theWindow.xhr.open('GET','http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/', true); 
-         theWindow.xhr.send(null);
+    //             }
+    //         };
+    //         theWindow.xhr.open('GET','http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/', true); 
+    //      theWindow.xhr.send(null);
             
               
             
             
-    },
-    
-     presser: function(imgA){
+    // },
+    //  setBackground: function (tumbHolder, url){
+    //      console.log(tumbHolder);
+    //         tumbHolder.addEventListener("click", function(){
+    //              var body = document.getElementsByTagName("body");
+    //              console.log(body);
+    //               body[0].style.backgroundImage = 'url('+url+')';
+    //         })
+         
+    //  }
+    // //  presser: function(imgA){
         
-            var body = document.getElementsByTagName('body')[0];
-                body.style.backgroundImage = "url("+imgA.URL+")";
+    // //         var body = document.getElementsByTagName('body')[0];
+    // //             body.style.backgroundImage = "url("+imgA.URL+")";
             
         
                 
            
-        },
-     }
+    // //     },
+    };
 // }
 
 window.onLoad = theWindow.setButtonPics();
